86fe86c4a4f92fc46c6a8b07a9f280d093326d80
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-17 00:52:15 +0100

    corrected header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,29 +8,33 @@ export default function Header() {
   return (
     <header className="fixed w-full z-10">
       <div className="px-8 py-4 flex justify-between items-center w-full">
-        <div className="flex items-center">
+        <div className="flex items-center w-1/6">
           <Link href="/">
             <img src="/logo.png" alt="Logo" className="h-16 w-16 mr-2" />
           </Link>
         </div>
 
-        <nav className="hidden md:flex space-x-10 justify-center w-full text-xl">
-          <Link href="/" className="text-white hover:text-lightOrange">
+        <nav className="hidden md:flex space-x-10 text-xl">
+          <Link href="/" className="text-white hover:text-darkGreen">
             O inwestycji
           </Link>
-          <Link href="/" className="text-white hover:text-lightOrange">
+          <Link href="/" className="text-white hover:text-darkGreen">
             Profity
           </Link>
-          <Link href="/" className="text-white hover:text-lightOrange">
+          <Link href="/" className="text-white hover:text-darkGreen">
             Domy
           </Link>
-          <Link href="/" className="text-white hover:text-lightOrange">
+          <Link href="/" className="text-white hover:text-darkGreen">
             Galeria
           </Link>
-          <Link href="/" className="text-white hover:text-lightOrange">
-            Kontakt
-          </Link>
         </nav>
+        <div className="w-1/6 flex justify-end">
+          <nav className=" hidden md:inline-block text-xl bg-darkOrange p-3 rounded-xl w-40 text-center">
+            <Link href="/" className="text-white hover:text-darkGreen">
+              Kontakt
+            </Link>
+          </nav>
+        </div>
 
         <button
           className="md:hidden flex items-center text-white"
@@ -57,35 +61,35 @@ export default function Header() {
         <div className="md:hidden flex flex-col items-center space-y-5 py-4 bg-darkGreen shadow-md">
           <Link
             href="/"
-            className="text-white hover:text-lightOrange hover:bg-darkOrange w-full flex justify-center"
+            className="text-white hover:bg-darkOrange w-full flex justify-center"
             onClick={() => setIsMenuOpen(false)}
           >
             O inwestycji
           </Link>
           <Link
             href="/"
-            className="text-white hover:text-lightOrange hover:bg-darkOrange w-full flex justify-center"
+            className="text-white hover:bg-darkOrange w-full flex justify-center"
             onClick={() => setIsMenuOpen(false)}
           >
             Profity
           </Link>
           <Link
             href="/"
-            className="text-white hover:text-lightOrange hover:bg-darkOrange w-full flex justify-center"
+            className="text-white hover:bg-darkOrange w-full flex justify-center"
             onClick={() => setIsMenuOpen(false)}
           >
             Domy
           </Link>
           <Link
             href="/"
-            className="text-white hover:text-lightOrange hover:bg-darkOrange w-full flex justify-center"
+            className="text-white hover:bg-darkOrange w-full flex justify-center"
             onClick={() => setIsMenuOpen(false)}
           >
             Galeria
           </Link>
           <Link
             href="/"
-            className="text-white hover:text-lightOrange hover:bg-darkOrange w-full flex justify-center"
+            className="text-white hover:bg-darkOrange w-full flex justify-center"
             onClick={() => setIsMenuOpen(false)}
           >
             Kontakt
