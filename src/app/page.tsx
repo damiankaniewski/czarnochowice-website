ba1a0cd97a761fbd6834eea3efeb622ef5ca8891
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-17 00:52:00 +0100

    added display of background rendered photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 export default function Home() {
   return (
-    <div>
+    <div className="bg-render1_mobile md:bg-render1 bg-cover bg-center h-screen">
       <Header />
       <main></main>
     </div>
