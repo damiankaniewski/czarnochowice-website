a8fd1416c75f2096a5aca4654d0b40b6bade511e
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-18 00:11:06 +0100

    improved header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Header from "@/components/Header";
 export default function Home() {
   return (
     <div className="bg-render1_mobile md:bg-render1 bg-cover bg-center h-screen">
+      <div className="absolute inset-0 bg-black opacity-30" />
+
       <Header />
       <main></main>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,30 +7,34 @@ export default function Header() {
 
   return (
     <header className="fixed w-full z-10">
-      <div className="px-8 py-4 flex justify-between items-center w-full">
+      <div
+        className={`px-8 py-4 flex justify-between items-center w-full bg-opacity-90 ${
+          isMenuOpen ? "bg-darkGreen" : "bg-transparent"
+        }`}
+      >
         <div className="flex items-center w-1/6">
           <Link href="/">
             <img src="/logo.png" alt="Logo" className="h-16 w-16 mr-2" />
           </Link>
         </div>
 
-        <nav className="hidden md:flex space-x-10 text-xl">
-          <Link href="/" className="text-white hover:text-darkGreen">
+        <nav className="hidden md:flex justify-center space-x-16 text-xl w-dvw">
+          <Link href="/" className="block text-white hover:text-lightOrange">
             O inwestycji
           </Link>
-          <Link href="/" className="text-white hover:text-darkGreen">
+          <Link href="/" className="block text-white hover:text-lightOrange">
             Profity
           </Link>
-          <Link href="/" className="text-white hover:text-darkGreen">
+          <Link href="/" className="block text-white hover:text-lightOrange">
             Domy
           </Link>
-          <Link href="/" className="text-white hover:text-darkGreen">
+          <Link href="/" className="block text-white hover:text-lightOrange">
             Galeria
           </Link>
         </nav>
         <div className="w-1/6 flex justify-end">
-          <nav className=" hidden md:inline-block text-xl bg-darkOrange p-3 rounded-xl w-40 text-center">
-            <Link href="/" className="text-white hover:text-darkGreen">
+          <nav className=" max-md:hidden text-xl bg-darkOrange p-3 rounded-xl w-40 text-center hover:bg-lightGreen">
+            <Link href="/" className="block text-white w-full">
               Kontakt
             </Link>
           </nav>
@@ -58,38 +62,38 @@ export default function Header() {
       </div>
 
       {isMenuOpen && (
-        <div className="md:hidden flex flex-col items-center space-y-5 py-4 bg-darkGreen shadow-md">
+        <div className="md:hidden flex flex-col items-center pt-4  bg-darkGreen bg-opacity-90 shadow-md">
           <Link
             href="/"
-            className="text-white hover:bg-darkOrange w-full flex justify-center"
+            className="text-white text-xl w-full p-5 flex justify-center hover:bg-lightGreen"
             onClick={() => setIsMenuOpen(false)}
           >
             O inwestycji
           </Link>
           <Link
             href="/"
-            className="text-white hover:bg-darkOrange w-full flex justify-center"
+            className="text-white text-xl w-full p-5 flex justify-center hover:bg-lightGreen"
             onClick={() => setIsMenuOpen(false)}
           >
             Profity
           </Link>
           <Link
             href="/"
-            className="text-white hover:bg-darkOrange w-full flex justify-center"
+            className="text-white text-xl w-full p-5 flex justify-center hover:bg-lightGreen"
             onClick={() => setIsMenuOpen(false)}
           >
             Domy
           </Link>
           <Link
             href="/"
-            className="text-white hover:bg-darkOrange w-full flex justify-center"
+            className="text-white text-xl w-full p-5 flex justify-center hover:bg-lightGreen"
             onClick={() => setIsMenuOpen(false)}
           >
             Galeria
           </Link>
           <Link
             href="/"
-            className="text-white hover:bg-darkOrange w-full flex justify-center"
+            className="text-white text-xl w-full p-5 flex justify-center hover:bg-lightGreen"
             onClick={() => setIsMenuOpen(false)}
           >
             Kontakt
