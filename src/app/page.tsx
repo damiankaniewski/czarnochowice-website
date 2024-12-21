eb089ce6373db660cd52bab6f9ec095492d8e39a
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-21 14:02:00 +0100

    feat: added section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import Header from "@/components/Header";
 import HomeSection from "@/components/HomeSection";
+import Investment from "@/components/Investment";
 
 export default function Home() {
   return (
     <div className=" h-screen w-full">
       <div className="bg-render1_mobile md:bg-render1 bg-cover bg-center h-screen w-full absolute opacity-60" />
-      <div className="h-full w-screen">
+      <div className="h-full w-full">
         <Header />
         <HomeSection />
+        <Investment />
       </div>
     </div>
   );
