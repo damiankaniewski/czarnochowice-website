e2045a11f1bd34be9db3bf211d947d94ce49dc60
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-22 02:41:32 +0100

    added display of house coordinates

--- src/components/Houses.tsx
+++ src/components/Houses.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import houseOffer from "@/config/houseOffer";
 import Link from "next/link";
 import { FaDownload } from "react-icons/fa6";
+
 export default function Houses() {
   return (
     <section
@@ -14,8 +15,8 @@ export default function Houses() {
         </p>
       </div>
       <div className="w-full px-10 pt-10 flex flex-row ">
-        <div className="w-3/4 flex justify-end items-start">
-          <div className="w-10/12">
+        <div className="w-3/4 flex justify-end items-start relative">
+          <div className="w-10/12 relative">
             <Image
               src="/renderZGory.jpg"
               alt="Estate"
@@ -25,14 +26,32 @@ export default function Houses() {
               width={533}
               height={400}
             />
+            {houseOffer.map((house, index) => (
+              <Link
+                href={house.linkDoOferty}
+                key={index}
+                className="absolute flex justify-center items-center text-white bg-green4 rounded-full text-sm font-bold"
+                style={{
+                  top: `${house.y}%`,
+                  left: `${house.x}%`,
+                  width: "30px",
+                  height: "30px",
+                  transform: "translate(-50%, -50%)",
+                }}
+              >
+                {house.numer}
+              </Link>
+            ))}
           </div>
         </div>
         <div className="w-1/2 flex justify-center items-start">
           <div className="w-3/4 flex flex-col justify-start items-center space-y-4 h-3/4 overflow-y-auto">
             {houseOffer.map((house, index) => (
-              <div className="flex flex-row w-full bg-white p-4 rounded-lg shadow-md space-y-2">
-                <div key={index} className="w-3/5 ">
-                  {" "}
+              <div
+                key={index}
+                className="flex flex-row w-full bg-white p-4 rounded-lg shadow-md space-y-2"
+              >
+                <div className="w-3/5">
                   <p className="text-green4 text-xl font-semibold">
                     Dom nr {house.numer}
                   </p>
@@ -58,7 +77,7 @@ export default function Houses() {
                 </div>
                 <div>
                   <Link
-                    className=" w-full bg-green2 p-4 rounded-xl text-white flex justify-center items-center flex-col "
+                    className="w-full bg-green2 p-4 rounded-xl text-white flex justify-center items-center flex-col"
                     href={house.linkDoOferty}
                   >
                     <FaDownload className="size-4" />
