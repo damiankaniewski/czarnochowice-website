3266029b368925ae9d295437a04e3338329259d2
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-22 22:06:42 +0100

    feat: completed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,37 @@ export default function Footer() {
     <footer>
       <div className="w-full h-full flex flex-col ">
         <div className="flex flex-row bg-green3 justify-between p-5">
-          <div className="flex flex-row justify-start">
+          <div className="flex flex-row justify-center items-center">
             <Link
               href="https://www.n20.pl/"
               target="_blank"
               rel="noopener noreferrer"
-              className="mr-8"
+              className="p-2"
             >
-              <Image src="/n20logo.png" alt="N20" width={192} height={120} />
+              <Image
+                src="/n20logo.png"
+                alt="N20"
+                width={212}
+                height={144}
+                className="p-2"
+              />
             </Link>
-            <Link href="https://www.n20.pl/">
-              {" "}
-              <Image src="/logo.png" alt="N20" width={120} height={120} />
+            <Link href="https://www.n20.pl/" className="p-2">
+              <Image
+                src="/logo.png"
+                alt="N20"
+                width={144}
+                height={144}
+                className="p-2"
+              />
             </Link>
           </div>
-          <div className="flex flex-col justify-end">
-            <div className="flex flex-row">
-              <Link href="https://www.facebook.com/N20RealEstate">
+          <div className="flex flex-col justify-center h-full items-center">
+            <div className="flex flex-row h-full w-full">
+              <Link
+                href="https://www.facebook.com/N20RealEstate"
+                className="p-2"
+              >
                 <Image
                   src="/Facebook_icon.png"
                   alt="Facebook"
@@ -30,7 +44,10 @@ export default function Footer() {
                   height={80}
                 />
               </Link>
-              <Link href="https://www.instagram.com/n20_nieruchomosci?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+              <Link
+                href="https://www.instagram.com/n20_nieruchomosci?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
+                className="p-2"
+              >
                 <Image
                   src="/Instagram_icon.png"
                   alt="Instagram"
@@ -39,12 +56,25 @@ export default function Footer() {
                 />
               </Link>
             </div>
-            <Link href="https://www.n20.pl/polityka-prywatnosci">
+            <Link
+              href="https://www.n20.pl/polityka-prywatnosci"
+              className="w-full h-full p-2 text-center text-lg mt-5"
+            >
               Polityka Prywatności
             </Link>
           </div>
         </div>
-        <div className="flex flex-row"></div>
+        <div className="flex flex-row justify-between items-center py-1 px-2 bg-signature text-white">
+          <p className="my-4">2024 Policrafts ©</p>
+          <div className="pr-5">
+            <p className="text-signature_text text-right p-0 m-0">
+              Wizualizacje: Maciej Krzyszkowski
+            </p>
+            <p className="text-signature_text text-right p-0 m-0">
+              Realizacja witryny: Rafał Ciupek
+            </p>
+          </div>
+        </div>
       </div>
     </footer>
   );
