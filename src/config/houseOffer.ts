97cb5058a49a0f491defca788eb282fc70778cc6
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-22 02:26:56 +0100

    feat: added sample offers

---
+++ src/config/houseOffer.ts
@@ -0,0 +1,44 @@
+const houseOffer = [
+  {
+    numer: "7a",
+    status: "Dostępny",
+    linkDoOferty: "#houses",
+    metraz: 54.21,
+    pokoje: 3,
+    cena: 598519,
+    poziom: "parter",
+    ogrodek: 38.41,
+  },
+  {
+    numer: "8a",
+    status: "Dostępny",
+    linkDoOferty: "#houses",
+    metraz: 54.21,
+    pokoje: 3,
+    cena: 598519,
+    poziom: "parter",
+    ogrodek: 38.41,
+  },
+  {
+    numer: "9a",
+    status: "Dostępny",
+    linkDoOferty: "#houses",
+    metraz: 54.21,
+    pokoje: 3,
+    cena: 598519,
+    poziom: "parter",
+    ogrodek: 38.41,
+  },
+  {
+    numer: "10a",
+    status: "Dostępny",
+    linkDoOferty: "#houses",
+    metraz: 54.21,
+    pokoje: 3,
+    cena: 598519,
+    poziom: "parter",
+    ogrodek: 38.41,
+  },
+];
+
+export default houseOffer;
