aa5b1a45ae0709c321b789ed45974aa97a736e3c
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-23 01:20:38 +0100

    feat: added animations to houses section

--- src/components/Houses.tsx
+++ src/components/Houses.tsx
@@ -25,6 +25,7 @@ export default function Houses() {
               className="rounded-lg shadow-lg w-1/2"
               width={533}
               height={400}
+              data-aos="fade-down"
             />
             {houseOffer.map((house, index) => (
               <Link
@@ -38,18 +39,24 @@ export default function Houses() {
                   height: "30px",
                   transform: "translate(-50%, -50%)",
                 }}
+                data-aos="fade-down"
+                data-aos-delay="200"
               >
                 {house.numer}
               </Link>
             ))}
           </div>
         </div>
-        <div className="w-full h-full md:w-1/2 flex justify-center items-start pt-10 md:pt-0">
+        <div
+          className="w-full h-full md:w-1/2 flex justify-center items-start pt-10 md:pt-0"
+          data-aos="fade-up"
+        >
           <div className="w-3/4  flex flex-col justify-start items-center space-y-4 h-full overflow-y-auto">
             {houseOffer.map((house, index) => (
               <div
                 key={index}
                 className="flex flex-row w-full bg-white p-4 rounded-lg shadow-md space-y-2"
+                data-aos-delay={`${index * 100}`}
               >
                 <div className="w-3/5">
                   <p className="text-green4 text-xl font-semibold">
