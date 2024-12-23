215ec69197873bd1b70d92bfaf7f57bc51c56aeb
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-23 01:09:30 +0100

    feat: added animations to profits section

--- src/components/Profits.tsx
+++ src/components/Profits.tsx
@@ -20,7 +20,10 @@ export default function Profits() {
       </div>
       <div className="flex w-full h-full flex-col p-10">
         <div className="flex w-full flex-col md:flex-row justify-center items-start h-full">
-          <div className="w-full h-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3">
+          <div
+            className="w-full h-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3"
+            data-aos="fade-up"
+          >
             <FaMapPin className="text-green4 text-5xl " />
             <p className="font-semibold text-lg text-center ">
               Blisko Krakowa i pełnej infrastruktury miejskiej
@@ -30,7 +33,11 @@ export default function Profits() {
               łatwym dostępem do autostrady.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
             <FaTree className="text-green4 text-5xl " />
             <p className="font-semibold text-lg text-center ">
               Spokój i natura z dala od miejskiego zgiełku
@@ -39,7 +46,11 @@ export default function Profits() {
               Zielona okolica łącząca ciszę i wygodę.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
             <FaCouch className="text-green4 text-5xl " />
             <p className="font-semibold text-lg text-center ">
               Przestronne wnętrza i nowoczesne rozwiązania architektoniczne
@@ -48,7 +59,11 @@ export default function Profits() {
               Domy z przestronnymi pokojami i funkcjonalnym układem.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
             <FaHeart className="text-green4 text-5xl " />
             <p className="font-semibold text-lg text-center ">
               Prywatne, ogrodzone działki
@@ -59,7 +74,11 @@ export default function Profits() {
           </div>
         </div>
         <div className="flex w-full flex-col md:flex-row justify-center items-start h-full md:pt-6">
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
             <FaSolarPanel className="text-green4 text-5xl" />
             <p className="font-semibold text-lg text-center">
               Pompa ciepła, rekuperacja, przygotowanie pod fotowoltaikę{" "}
@@ -68,7 +87,11 @@ export default function Profits() {
               Energooszczędne technologie redukujące koszty.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="500"
+          >
             <FaTemperatureHalf className="text-green4 text-5xl" />
             <p className="font-semibold text-lg text-center">
               Wysoka izolacja termiczna (50 cm ściany)
@@ -77,14 +100,22 @@ export default function Profits() {
               Ściany o wyjątkowej izolacji gwarantujące oszczędność energii.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
             <FaLock className="text-green4 text-5xl" />
             <p className="font-semibold text-lg text-center">Zamknięty teren</p>
             <p className="text-sm text-center">
               Ogrodzona przestrzeń z przesuwaną bramą wjazdową i furtką.
             </p>
           </div>
-          <div className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5">
+          <div
+            className="w-full md:w-1/4 flex flex-col text-green4 justify-center items-center px-4 space-y-3 pt-5"
+            data-aos="fade-up"
+            data-aos-delay="700"
+          >
             <FaHouseChimneyWindow className="text-green4 text-5xl" />
             <p className="font-semibold text-lg text-center">
               Elektryczne rolety
