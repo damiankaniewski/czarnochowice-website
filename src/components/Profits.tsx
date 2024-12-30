6ffe22011564868cc282a23d361dc872d21cded2
ruffaaw <rafal.p.ciupek@gmail.com>
2024-12-30 22:12:11 +0100

    style: improved header in profits section

--- src/components/Profits.tsx
+++ src/components/Profits.tsx
@@ -14,7 +14,7 @@ export default function Profits() {
       className="w-full h-full flex flex-col items-center bg-grey relative scroll-mt-20"
     >
       <div>
-        <p className="text-6xl text-green4 pt-10 font-bold break-words max-w-full">
+        <p className="text-3xl sm:text-4xl lg:text-6xl text-green4 pt-10 font-bold break-words max-w-full">
           PROFITY
         </p>
       </div>
