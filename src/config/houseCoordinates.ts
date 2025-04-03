ce3437a3e3ab9a180b88b8ffa6bed8fdb13d0f08
ruffaaw <rafalciupek07@gmail.com>
2025-04-03 18:21:26 +0200

    fix: updated houses section

--- src/config/houseCoordinates.ts
+++ src/config/houseCoordinates.ts
@@ -1,8 +1,8 @@
 const houseCoordinates = [
-  { numer: "dom1", x: 38, y: 12 },
-  { numer: "dom2", x: 40, y: 18 },
-  { numer: "dom3", x: 49, y: 40 },
-  { numer: "dom4", x: 54, y: 53 },
+  { numer: "dom1", x: 36, y: 9 },
+  { numer: "dom2", x: 40, y: 15 },
+  { numer: "dom3", x: 53, y: 41 },
+  { numer: "dom4", x: 60, y: 55 },
 ];
 
 export default houseCoordinates;
